30764b1f22f05b8232a3e1b868fe8c0f775e54c9
Peter Tran <peter.tran@uclmail.net>
2014-11-22 16:00:04 +0000

    Update.

--- src/main/scala/dao/DBConnection.scala
+++ src/main/scala/dao/DBConnection.scala
@@ -17,31 +17,9 @@ object DBConnection {
     	this.password = new String(System.console.readPassword)
     }
     
+    def getConnection(): Connection = {
+        DriverManager.getConnection(DBConnection.url, DBConnection.username, DBConnection.password)
+	}
+    
     override def toString() = this.driver + " | " + this.url + " | " + this.username + " | " + this.password
 }
-
-/*
-    val driver = "com.mysql.jdbc.Driver"
-    val url = "jdbc:mysql://localhost/world"
-    val username = "root"
-    val password = "Wbgs9906"
-    
-    var connection:Connection = null
-    
-    try {
-      // Make the connection
-      Class.forName(driver)
-      connection = DriverManager.getConnection(url, username, password)
- 
-      // Create the statement, and run the select query
-      val statement = connection.createStatement()
-      val resultSet = statement.executeQuery("SELECT * FROM CITY WHERE CountryCode = 'GBR' LIMIT 1")
-      while ( resultSet.next() ) {
-        val name = resultSet.getString("Name")
-        val countryCode = resultSet.getString("CountryCode")
-        println("Name, CountryCode = " + name + ", " + countryCode)
-      }
-    } catch {
-      case e => e.printStackTrace
-    }
-    connection.close()*/
