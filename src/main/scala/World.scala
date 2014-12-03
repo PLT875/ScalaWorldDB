113da6dbd40aebd9fd62218e5fabaee772ba33db
Peter Tran <peter.tran@uclmail.net>
2014-12-03 20:51:08 +0000

    Formatting

--- src/main/scala/World.scala
+++ src/main/scala/World.scala
@@ -1,114 +1,100 @@
-import java.util.{Date, Locale}
-import java.text.{DateFormat, SimpleDateFormat}
+import java.util.{ Date, Locale }
+import java.text.{ DateFormat, SimpleDateFormat }
 import java.sql.{ DriverManager, Connection, ResultSet }
 import dao._
 import model._
 import analytics._
 
 /*
- * Author: Peter Tran
- * 
- * This application is for learning purposes, as an introduction to Scala.
- * 
- * Resources
- * ++ MySQL world database: http://dev.mysql.com/doc/index-other.html
- * ++ Functional Programming Principles in Scala: https://www.coursera.org/course/progfun
- * ++ Programming in Scala, First Edition: http://www.artima.com/pins1ed/
- * 
- */
+* Author: Peter Tran
+* 
+* This application is for learning purposes, as an introduction to Scala.
+* 
+* Resources
+* ++ MySQL world database: http://dev.mysql.com/doc/index-other.html
+* ++ Functional Programming Principles in Scala: https://www.coursera.org/course/progfun
+* ++ Programming in Scala, First Edition: http://www.artima.com/pins1ed/
+* 
+*/
 
- object World extends App {
-    val dateFormat = new SimpleDateFormat("yyyy-MM-dd").format(new Date)
-    println(s"*** $dateFormat: Getting Started with Scala - World Database Example ***")
+object World extends App {
 
-    // Set user name and password for database connection
-    DBConnection.username; DBConnection.password
-    
-    menu
+	val dateFormat = new SimpleDateFormat("yyyy-MM-dd").format(new Date)
+	println(s"*** $dateFormat: Getting Started with Scala - World Database Example ***")
 
-    def menu(): Unit = {
-        println("")
-        println("--- Options")
-        println("--- a. List Cities By Country")
-        println(" -- b. List Population Density By Region")
-        println("--- x. Exit")
-        val option : String = readLine("Enter an option: ")
+	// Set user name and password for database connection
+	DBConnection.username; DBConnection.password
+	menu
+	
+	def menu(): Unit = {
+		println("")
+		println("--- Options")
+		println("--- a. List Cities By Country")
+		println(" -- b. List Population Density By Region")
+		println("--- x. Exit")
+		val option: String = readLine("Enter an option: ")
 
-        try {
-            matchChoice(option)
-            } catch {
-               case e : Throwable => println(e.getMessage)
-           }  
-
-           def matchChoice(choice: String): Unit = choice match {
-            case "a" => listCities; menu
-            case "b" => listPopulationDensityByRegion; menu
-            case "x"=> return
-            case _ => println(s"Unknown option: $choice"); menu
-        }
-        
-        def listCities() : Unit = {
-            println("")
-            
-            val countryCode = readLine("Enter a country code: ")
-            val cityDao = new CityDao
-            val cities = cityDao.getCities(countryCode)
-            cities.foreach(println)
-            println("")
-            val yn = readLine("List population by district [y or n]: ")
-            if(yn equals "y"){
-                val popByDistrict = CityAnalytics.populationByDistrict(cities)
-                popByDistrict.foreach { case (key, value) => println ("(" + key + ", " + value + ")") }
-            }
-
-        }
-
-        def  listPopulationDensityByRegion(): Unit = {
-            val region = readLine("Enter a region e.g. Eastern Asia: ")
-            val countryDao = new CountryDao
-            val countries = countryDao.getCountries(region)
-            val popDensity = CountryAnalytics.populationDensity(countries)
-            popDensity.sortWith((c1, c2) => c1._2 > c2._2).foreach(println)
-        }
-
-
-    }
-    
-    /*
-    
-    try {
-      
-    	
-   
-    	
-    	println("")
-    	println("*** Population Density of Countries in Southeast or Eastern Asia ***")
-    	val countryDao = new CountryDao
-    	val countries = countryDao.getCountries("'Southeast Asia' or Region = 'Eastern Asia'")
-    	val popDensity = CountryAnalytics.populationDensity(countries)
-    	popDensity.sortWith((c1, c2) => c1._2 > c2._2).foreach(println)
-    	
-    	println("")
-    	println("*** Switzerland Statistics ***")
-    	val chePopulation = countryDao.getPopulation("Switzerland")
-    	println(s"Population: $chePopulation")
-    	
-    	val countryLanguageDao = new CountryLanguageDao
-    	val cheLanguages = countryLanguageDao.getCountryLanguage("CHE")
-
-    	println("Population of Spoken Languages")
-    	CountryLanguageAnalytics.languagePopulation(cheLanguages, chePopulation).foreach(println)
-    	
-    	println("*** Population by District - Indonesia ***")
-    	val citiesIdn = cityDao.getCities("IDN");
-    	val popByDistrictIdn = CityAnalytics.populationByDistrict(citiesIdn).filter(x => (x._1.contains("Java")))
-    	popByDistrictIdn.foreach { case (key, value) => println ("(" + key + ", " + value + ")") }
-    	
-    } catch {
-	    case e : Throwable => println(e.getMessage)
+		try {
+			matchChoice(option)
+		} catch {
+			case e: Throwable => println(e.getMessage)
+		}
+	}
+		
+	def matchChoice(choice: String): Unit = choice match {
+		case "a" => listCities; menu
+		case "b" => listPopulationDensityByRegion; menu
+		case "x" => return
+		case _ => println(s"Unknown option: $choice"); menu
 	}
 	
-	**/
-
-
+	def listCities(): Unit = {
+		println("")
+		val countryCode = readLine("Enter a country code: ")
+		val cityDao = new CityDao
+		val cities = cityDao.getCities(countryCode)
+		cities.foreach(println)
+		println("")
+		val yn = readLine("List population by district [y or n]: ")
+		if (yn equals "y") {
+			val popByDistrict = CityAnalytics.populationByDistrict(cities)
+			popByDistrict.foreach { 
+				case (key, value) => println("(" + key + ", " + value + ")") 
+			}
+		}
+	}
+	
+	def listPopulationDensityByRegion(): Unit = {
+		val region = readLine("Enter a region e.g. Eastern Asia: ")
+		val countryDao = new CountryDao
+		val countries = countryDao.getCountries(region)
+		val popDensity = CountryAnalytics.populationDensity(countries)
+		popDensity.sortWith((c1, c2) => c1._2 > c2._2).foreach(println)
+	}
+        
 }
+
+
+
+
+
+
+/*
+
+val countryLanguageDao = new CountryLanguageDao
+val cheLanguages = countryLanguageDao.getCountryLanguage("CHE")
+
+println("Population of Spoken Languages")
+CountryLanguageAnalytics.languagePopulation(cheLanguages, chePopulation).foreach(println)
+
+println("*** Population by District - Indonesia ***")
+val citiesIdn = cityDao.getCities("IDN");
+val popByDistrictIdn = CityAnalytics.populationByDistrict(citiesIdn).filter(x => (x._1.contains("Java")))
+popByDistrictIdn.foreach { case (key, value) => println ("(" + key + ", " + value + ")") }
+
+} catch {
+case e : Throwable => println(e.getMessage)
+}
+
+**/
+
--- src/main/scala/analytics/CountryLanguageAnalytics.scala
+++ src/main/scala/analytics/CountryLanguageAnalytics.scala
@@ -2,6 +2,7 @@ package analytics
 import model._
 
 object CountryLanguageAnalytics {
+	
 	def languagePopulation(countryLanguages : List[CountryLanguage], countryPopulation : Int): List[(String, Double)] = {
 		val langPercentage = countryLanguages.map(cl => (cl.language, cl.percentage))
 		val langPopulation = langPercentage.map( cl => (cl._1, (cl._2 / 100) * countryPopulation))
